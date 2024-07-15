0638da3626eba5e753b9034ccacfdd49b220df1f
조병민 <jsjbm@naver.com>
2024-07-15 20:20:30 +0900

    # 2주차 에셋 추가 - SPUM 미사용 스크립트 제거 - Missing Script를 제거하는 기능 추가     - [Window] - [미싱 스크립트 제거]
    ## 이슈
    - SPUM Boss_Monster_000 애니메이션 오류
        - 말 전용 애니메이션 클립, 컨트롤러 필요

---
+++ Assets/Editor/RemoveMissing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RemoveMissing : Editor
+{
+    [MenuItem("Window/미싱 스크립트 제거", false, 1231)]
+    private static void RemoveMissings()
+    {
+        var select = Selection.gameObjects;
+        int componentCount = 0;
+        int gameObjectCount = 0;
+
+        foreach (var gameObject in select)
+        {
+            var missingScriptCount = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(gameObject);
+
+            if (missingScriptCount > 0)
+            {
+                Undo.RegisterCompleteObjectUndo(gameObject, "미싱 크스립트 제거");
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
+
+                componentCount += missingScriptCount;
+                gameObjectCount++;
+            }
+        }
+
+        Debug.Log($"{gameObjectCount}개의 게임 오브젝트에서 {componentCount}개의 미싱스크립트 제거 완료");
+    }
+}
