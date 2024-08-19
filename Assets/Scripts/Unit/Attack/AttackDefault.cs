c2545c159b76f3f38909109fa107b696a96a36f3
조병민 <jsjbm@naver.com>
2024-08-19 13:52:51 +0900

    # 오류 수정 - 타겟이 없을 때도 공격 메서드가 실행되는 오류 수정

--- Assets/Scripts/Unit/Attack/AttackDefault.cs
+++ Assets/Scripts/Unit/Attack/AttackDefault.cs
@@ -6,7 +6,10 @@ public class AttackDefault : IAttackStrategy
 {
     public bool Attack(IDamagedable target, int damage, ATTACK_TYPE type = ATTACK_TYPE.NONE)
     {
-        return target.TakeDamage(damage, type).Item1;
+        if (target != null)
+            return target.TakeDamage(damage, type).Item1;
+        else
+            return false;
     }
 
     public bool Attack(Vector3 targetPos, int damage, ATTACK_TYPE type = ATTACK_TYPE.NONE)
