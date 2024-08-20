44fc0e6e19af08901459cb2a6a1feb2d9fc6d345
장수진 <jsooj115@gmail.com>
2024-08-20 09:52:37 +0900

    - 유닛 터치 ui 이름, 파라미터 바 갱신되도록 수정 - 유닛 등급 아이콘 addressable등록 - 유닛이 현재 사용 가능 타일로만 이동하도록 수정

--- Assets/Scripts/UI/Window/UITouchUnitButtons.cs
+++ Assets/Scripts/UI/Window/UITouchUnitButtons.cs
@@ -1,12 +1,30 @@
-﻿using UnityEngine.UI;
+﻿using TMPro;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using System.ComponentModel;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 public class UITouchUnitButtons : UIWindow
 {
     public override WINDOW_NAME WindowName => WINDOW_NAME.TOUCH_UNIT_BUTTONS;
 
+    public Image gradeImage;
+    public TextMeshProUGUI unitName;
+
+    public Slider hpBar;
+    public Slider staminaBar;
+    public Slider mentalBar;
+
     public Button information;
     public Button close;
     public Button placement;
+
+    private UIManager um;
+    private string path = "Assets/Pick_Asset/2WEEK/GradeIcon/Grade_";
+    private List<Sprite> gradeIcons = new();
 
     protected override void Awake()
     {
@@ -18,9 +36,60 @@ public class UITouchUnitButtons : UIWindow
         base.OnGameStart();
         isShowOnly = false;
 
+        um = GameManager.uiManager;
+
+        for(int i = 1; i <= 5; ++i)
+        {
+            var newpath = $"{path}{i}.png";
+            Debug.Log(newpath);
+            Addressables.LoadAssetAsync<Sprite>(newpath).Completed += OnLoadDone;
+        }
+
         information.onClick.AddListener(OnButtonInformation);
         close.onClick.AddListener(OnButtonClose);
         placement.onClick.AddListener(OnButtonPlacement);
+    }
+
+    private void OnLoadDone(AsyncOperationHandle<Sprite> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            gradeIcons.Add(obj.Result);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SetUI();
+    }
+
+    private void SetUI()
+    {
+        SetText();
+        SetGradeIcon();
+    }
+
+    private void SetText()
+    {
+        unitName.text = um.currentUnitStats.Data.Name;
+    }
+
+    private void SetGradeIcon()
+    {
+        gradeImage.sprite = gradeIcons[(int)um.currentUnitStats.UnitGrade];
+    }
+
+    private void SetParameterBar()
+    {
+        var stats = um.currentUnitStats;
+        hpBar.value = stats.HP.Current / stats.HP.max;
+        staminaBar.value = stats.Stamina.Current / stats.Stamina.max;
+        mentalBar.value = stats.Stress.Current / stats.Stress.max;
+    }
+
+    private void Update()
+    {
+        SetParameterBar();
     }
 
     private void OnButtonInformation()
