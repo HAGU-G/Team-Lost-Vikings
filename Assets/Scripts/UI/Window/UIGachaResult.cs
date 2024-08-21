847d39994539f261af6ad4f6eb46821505854f18
장수진 <jsooj115@gmail.com>
2024-08-21 18:47:47 +0900

    - 뽑기 결과 UI 수정

--- Assets/Scripts/UI/Window/UIGachaResult.cs
+++ Assets/Scripts/UI/Window/UIGachaResult.cs
@@ -1,5 +1,11 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
+using static UnityEngine.UI.CanvasScaler;
 
 public class UIGachaResult : UIWindow
 {
@@ -9,23 +15,80 @@ public class UIGachaResult : UIWindow
     public Image gradeICon;
     public RawImage unitImage;
     public TextMeshProUGUI unitName;
+    public Button information;
+    public Button reRecruit;
 
     public Button exit;
-    //public UnitStats gachaResultUnit;
+
+    private Dictionary<int, Sprite> gradeIcons = new();
 
     private void OnEnable()
     {
         if (!IsReady)
             return;
     }
+    protected override void OnGameStart()
+    {
+        base.OnGameStart();
+
+        var cnt = Enum.GetValues(typeof(UNIT_GRADE)).Length;
+        for (int i = 0; i <= cnt; ++i)
+        {
+            var path = $"Grade_0{i + 1}";
+            var id = i;
+            Addressables.LoadAssetAsync<Sprite>(path).Completed += (obj) => OnLoadDone(obj, id);
+        }
+        information.onClick.AddListener(OnButtonInformation);
+        exit.onClick.AddListener(OnButtonExit);
+    }
+
+    private void OnLoadDone(AsyncOperationHandle<Sprite> obj, int id)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            gradeIcons.Add(id, obj.Result);
+        }
+    }
 
     public void SetResult(UnitStats unitStats)
     {
-        gachaResultDesc.text = $"{unitStats.UnitGrade.ToString()} 등급의 {unitStats.Data.Name}를 뽑았습니다.";
-        //unitImage.sprite = GameManager.uiManager.unitRenderTexture.LoadRenderTexture(gachaResultUnit.AssetFileName);
+        foreach (var u in GameManager.unitManager.Units.Values)
+        {
+            if (u.Id == unitStats.Id)
+            {
+                gachaResultDesc.text = $"보유 중인 모험가가 모집되어 대기소에 배치되었습니다.";
+                reRecruit.GetComponentInChildren<TextMeshProUGUI>().text = "대기소로";
+                reRecruit.onClick.RemoveAllListeners();
+                reRecruit.onClick.AddListener(
+                    () => GameManager.uiManager.windows[WINDOW_NAME.CHARACTER_STASH].Open());
+            }
+        }
+
+        if (GameManager.unitManager.unitLimitCount < GameManager.unitManager.Units.Count + 1)
+        {
+            gachaResultDesc.text = $"자리가 없어서 대기소에 배치되었습니다.";
+            reRecruit.GetComponentInChildren<TextMeshProUGUI>().text = "대기소로";
+            reRecruit.onClick.RemoveAllListeners();
+            reRecruit.onClick.AddListener(
+                () => GameManager.uiManager.windows[WINDOW_NAME.CHARACTER_STASH].Open());
+        }
+        else
+        {
+            gachaResultDesc.text = $"새로운 모험가가 합류했습니다."; 
+            reRecruit.GetComponentInChildren<TextMeshProUGUI>().text = "다시 모집";
+            reRecruit.onClick.RemoveAllListeners();
+            reRecruit.onClick.AddListener(
+                () => GameManager.uiManager.windows[WINDOW_NAME.GACHA_UI].Open());
+        }
+
         unitImage.uvRect = GameManager.uiManager.unitRenderTexture.LoadRenderTexture(unitStats.Data.UnitAssetFileName);
-        gradeICon.sprite = GameManager.uiManager.gradeIcons[(int)unitStats.UnitGrade];
+        gradeICon.sprite = gradeIcons.GetValueOrDefault((int)unitStats.UnitGrade);
         unitName.text = unitStats.Data.Name;
+    }
+
+    private void OnButtonInformation()
+    {
+        GameManager.uiManager.windows[WINDOW_NAME.UNIT_DETAIL_INFORMATION].Open();
     }
 
     public void OnButtonExit()
