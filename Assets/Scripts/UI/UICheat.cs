5f8a67420770ac4d23c6612a8a6976e5bdc057e9
조병민 <jsjbm@naver.com>
2024-08-23 11:42:42 +0900

    # 치트용 UI 오브젝트 스크립트 추가

---
+++ Assets/Scripts/UI/UICheat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class UICheat : MonoBehaviour
+{
+    public Button buttonGold;
+    public Button buttonAll;
+    public Button buttonLevel;
+
+    private void Awake()
+    {
+        buttonGold.onClick.AddListener(() =>
+        { 
+            GameManager.itemManager?.CheatGold(5000);
+        });
+        buttonAll.onClick.AddListener(() =>
+        {
+            GameManager.itemManager?.CheatAllItem(5000);
+        });
+        buttonLevel.onClick.AddListener(() =>
+        {
+            GameManager.itemManager?.CheatLevel(5000);
+        });
+    }
+}
